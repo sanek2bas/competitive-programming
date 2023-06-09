99a5bf958183de0772f77a65362b527f39210a85
sanek2bas <sanek_2bas@mail.ru>
2023-06-09 12:49:52 +0300

    binary search task

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,22 +1,11 @@
-﻿using Interview.HashTable;
+﻿using Interview.BinarySearch;
+using Interview.HashTable;
 using Interview.LinkedList;
 using Interview.PrefixSum;
 
-//foreach(var test in MinimumSizeSubarraySum.GetTests())
-//{
-//    var answer = MinimumSizeSubarraySum.Execute(test.target, test.numbers);
-//    Console.WriteLine(answer == test.answer);
-//}
-
-//foreach (var test in MergeSortedLists.GetTests())
-//{
-//    var answer = MergeSortedLists.Execute(test.nodes);
-//    Console.WriteLine(MergeSortedLists.ConvertToString(answer) == test.answer);
-//}
-
-foreach (var test in SingleNumber.GetTests())
+foreach (var test in BinarySearch.GetTests())
 {
-    var answer = SingleNumber.Execute(test.numbers);
+    var answer = BinarySearch.Execute(test.numbers, test.target);
     Console.WriteLine(answer == test.answer);
 }
 
---
+++ yandex/Interview/BinarySearch/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿namespace Interview.BinarySearch
+{
+    public static class BinarySearch
+    {
+        /// <summary>
+        /// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+        /// If target exists, then return its index. Otherwise, return -1.
+        /// You must write an algorithm with O(log n) runtime complexity.
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int Execute(int[] numbers, int target)
+        {
+            int min = 0;
+            int max = numbers.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (numbers[mid] == target)
+                    return mid;
+                if (numbers[mid] > target)
+                    max = mid - 1;
+                else
+                    min = mid + 1;
+            }
+            return -1;
+        }
+
+        public static IEnumerable<(int[] numbers, int target, int answer)> GetTests()
+        {
+            yield return (new int[] { -1, 0, 3, 5, 9, 12 }, 9, 4);
+            yield return (new int[] { -1, 0, 3, 5, 9, 12 }, 2, -1);
+        }
+    }
+}
