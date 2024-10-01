922fd0bc037d6bf6302f25b573a07b44d6b6033e
Alexander Gordeev <a.gordeev@trbonet.com>
2024-10-01 17:39:53 +0300

    ReverseWordsInString

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,7 +14,6 @@ using TopInterview150.Divide_Conquer;
 using TopInterview150._1D_DP;
 using TopInterview150.TwoPointers;
 using TopInterview150.Matrix;
-using Multitrack;
 using Route256.Sandbox;
 using Route256.Contest;
 using TopInterview150.GraphGeneral;
@@ -27,10 +26,8 @@ using TopInterview150.Kadane_sAlgorithm;
 
 
 
-foreach (var test in MaximumSubarray.GetTests())
+foreach (var test in ReverseWordsInString.GetTests())
 {
-    var result = MaximumSubarray.Execute(test.nums);
-    Console.WriteLine(MaximumSubarray.CheckResult(result, test.answer));
+    var result = ReverseWordsInString.Execute(test.s);
+    Console.WriteLine(ReverseWordsInString.CheckResult(result, test.answer));
 }
-
-Console.ReadKey();
---
+++ TopInterview150/Array&String/ReverseWordsInString.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace TopInterview150.Array_String
+{
+    public static class ReverseWordsInString
+    {
+        /// <summary>
+        /// Given an input string s, reverse the order of the words.
+        /// A word is defined as a sequence of non-space characters.The words in s will be separated by at least one space.
+        /// Return a string of the words in reverse order concatenated by a single space.
+        /// Note that s may contain leading or trailing spaces or multiple spaces between two words.
+        /// The returned string should only have a single space separating the words. Do not include any extra spaces.
+        /// </summary>
+        public static string Execute(string s)
+        {
+            s = s.Trim();
+            var result = new StringBuilder();
+            var word = new StringBuilder();
+            for(int i = s.Length - 1; i >=0; i--)
+            {
+                if(s[i] == ' ')
+                {
+                    if(word.Length > 0)
+                    {
+                        Reverse(word);
+                        result.Append(word.ToString());
+                        result.Append(' ');
+                        word.Clear();
+                    }
+                }
+                else
+                {
+                    word.Append(s[i]);
+                }
+            }
+            if(word.Length > 0)
+            {
+                Reverse(word);
+                result.Append(word.ToString());
+                word.Clear();
+            }
+
+            return result.ToString();
+        }
+
+        private static void Reverse(StringBuilder sb)
+        {
+            int left = 0;
+            int right = sb.Length - 1;
+            while(left < right)
+            {
+                var temp = sb[left];
+                sb[left] = sb[right];
+                sb[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        public static IEnumerable<(string s, string answer)> GetTests()
+        {
+            yield return ("the sky is blue", "blue is sky the");
+            yield return("  hello world  ", "world hello");
+            yield return("a good   example", "example good a");
+        }
+
+        public static bool CheckResult(string result, string answer)
+        {
+            return answer == result;
+        }
+    }
+}
