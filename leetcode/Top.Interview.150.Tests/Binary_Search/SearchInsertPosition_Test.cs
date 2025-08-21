e4a59c00f1e0f522b0cae183e998f8acd52744eb
TRBONET\a.gordeev <a.gordeev@trbonet.com>
2025-08-21 16:26:44 +0300

    SearchInsertPosition_Test

---
+++ leetcode/Top.Interview.150.Tests/Binary_Search/SearchInsertPosition_Test.cs
@@ -0,0 +1,18 @@
+﻿using Top.Interview._150.Binary_Search;
+
+namespace Top.Interview._150.Tests
+{
+    public class SearchInsertPosition_Test
+    {
+        [Test]
+        [Arguments(new int[] { 1, 3, 5, 6 }, 5, 2)]
+        [Arguments(new int[] { 1, 3, 5, 6 }, 2, 1)]
+        [Arguments(new int[] { 1, 3, 5, 6 }, 7, 4)]
+        public void SearchInsertPosition(int[] nums, int target, int answer)
+        {
+            var solution = new SearchInsertPosition();
+            var result = solution.Execute(nums, target);
+            Assert.Equals(result, answer);
+        }
+    }
+}
---
+++ leetcode/Top.Interview.150/Binary_Search/SearchInsertPosition.cs
@@ -0,0 +1,22 @@
+﻿namespace Top.Interview._150.Binary_Search
+{
+    public class SearchInsertPosition
+    {
+        public int Execute(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length;
+            while (left < right)
+            {
+                int middle = (left + right) / 2;
+                if (nums[middle] == target)
+                    return middle;
+                if (nums[middle] < target)
+                    left = middle + 1;
+                else
+                    right = middle;
+            }
+            return left;
+        }
+    }
+}
