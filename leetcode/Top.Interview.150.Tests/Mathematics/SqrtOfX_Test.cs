cf9838fd7bef71d6a05e1da759082ae7afd5ebb9
sanek2bas <sanek_2bas@mail.ru>
2025-08-23 00:00:08 +0300

    SqrtOfX

---
+++ leetcode/Top.Interview.150.Tests/Mathematics/SqrtOfX_Test.cs
@@ -0,0 +1,20 @@
+﻿using Top.Interview._150.Mathematics;
+using TUnit.Assertions;
+using TUnit.Assertions.Extensions;
+using TUnit.Core;
+
+namespace Mathematics;
+
+public class SqrtOfX_Test
+{
+    [Test]
+    [Arguments(4, 2)]
+    [Arguments(8, 2)]
+    [Arguments(2147483647, 46340)]
+    public async Task SqrtOfX(int x, int answer)
+    {
+        var solution = new SqrtOfX();
+        var result = solution.Execute(x);
+        await Assert.That(result).IsEqualTo(answer);
+    }
+}
---
+++ leetcode/Top.Interview.150/Mathematics/SqrtOfX.cs
@@ -0,0 +1,26 @@
+﻿namespace Top.Interview._150.Mathematics;
+
+public class SqrtOfX
+{
+    /// <summary>
+    /// # 69
+    /// https://leetcode.com/problems/sqrtx/description/
+    /// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
+    /// The returned integer should be non-negative as well.
+    /// You must not use any built-in exponent function or operator.
+    /// </summary>
+    public int Execute(int x)
+    {
+        uint left = 1;
+        uint right = (uint)x + 1;
+        while (left < right)
+        {
+            uint middle = (left + right) / 2;
+            if (middle > (x / middle))
+                right = middle;
+            else
+                left = middle + 1;
+        }
+        return (int)left - 1;
+    }
+}
