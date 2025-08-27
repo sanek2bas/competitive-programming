c74009f2130f089313e92ec3c8eddf858f4ca7de
TRBONET\a.gordeev <a.gordeev@trbonet.com>
2025-08-27 11:27:47 +0300

    refactoring

--- leetcode/Top.Interview.150/Trie/ImplementTriePrefixTree.cs
+++ leetcode/Top.Interview.150/Trie/ImplementTriePrefixTree.cs
@@ -3,6 +3,8 @@ namespace Top.Interview._150.Trie;
 public class ImplementTriePrefixTree
 {
     /// <summary>
+    /// # 28
+    /// https://leetcode.com/problems/implement-trie-prefix-tree/description/
     /// A trie (pronounced as "try") or prefix tree is a tree data structure 
     /// used to efficiently store and retrieve keys in a dataset of strings. 
     /// There are various applications of this data structure, 
