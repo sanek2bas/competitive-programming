55a879f53fb406b08e89f5ccd3fb6cf9daf74545
sanek2bas <sanek_2bas@mail.ru>
2025-08-30 22:39:32 +0300

    MinimumSizeSubarraySum

---
+++ leetcode/Top.Interview.150.Tests/SlidingWindow/MinimumSizeSubarraySum_Test.cs
@@ -0,0 +1,20 @@
+﻿using Top.Interview._150.SlidingWindow;
+using TUnit.Assertions;
+using TUnit.Assertions.Extensions;
+using TUnit.Core;
+
+namespace SlidingWindow;
+
+public class MinimumSizeSubarraySum_Test
+{
+    [Test]
+    [Arguments(7, new int[] { 2, 3, 1, 2, 4, 3 }, 2)]
+    [Arguments(4, new int[] { 1, 4, 4 }, 1)]
+    [Arguments(11, new int[] { 1, 4, 4 }, 0)]
+    public async Task MinimumSizeSubarraySum(int target, int[] nums, int answer)
+    {
+        var solution = new MinimumSizeSubarraySum();
+        var result = solution.Execute(target, nums);
+        await Assert.That(result).IsEquivalentTo(answer);
+    }
+}
