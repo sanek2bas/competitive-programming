dd6f04d8eeade0b68481a674bfc48133a8bde9dc
sanek2bas <sanek_2bas@mail.ru>
2025-09-08 15:42:21 +0300

    Combination

---
+++ leetcode/Top.Interview.150.Tests/Backtracking/Combination_Test.cs
@@ -0,0 +1,34 @@
+﻿using Top.Interview._150.Backtracking;
+
+namespace Backtracking;
+
+public class Combination_Test
+{
+    [Test]
+    [MethodDataSource(nameof(DataSource))]
+    public async Task Combination(int n, int k, IList<IList<int>> answer)
+    {
+        var solution = new Combination();
+        var result = solution.Execute(n, k);
+        await Assert.That(result).IsEquivalentTo(answer);
+    }
+
+    public IEnumerable<(int n, int k, IList<IList<int>> answer)> DataSource()
+    {
+        yield return (4, 2,
+            new List<IList<int>>
+            {
+                new List<int> { 1, 2 },
+                new List<int> { 1, 3 },
+                new List<int> { 1, 4 },
+                new List<int> { 2, 3 },
+                new List<int> { 2, 4 },
+                new List<int> { 3, 4 }
+            });
+        yield return (1, 1, 
+            new List<IList<int>> 
+            { 
+                new List<int> { 1 } 
+            });
+    }
+}
