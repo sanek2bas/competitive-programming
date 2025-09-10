f98d262b7977c531bd8ad160f612881e57d2f940
sanek2bas <sanek_2bas@mail.ru>
2025-09-10 14:51:11 +0300

    ValidAnagram

---
+++ leetcode/Top.Interview.150/Hashmap/ValidAnagram.cs
@@ -0,0 +1,31 @@
+﻿namespace Top.Interview._150.Hashmap;
+
+public class ValidAnagram
+{
+    /// <summary>
+    /// # 242
+    /// https://leetcode.com/problems/valid-anagram/
+    /// Given two strings s and t, return true 
+    /// if t is an anagram of s, and false otherwise.
+    /// An Anagram is a word or phrase formed by 
+    /// rearranging the letters of a different word or phrase, 
+    /// typically using all the original letters exactly once.
+    /// </summary>
+    public bool Execute(string s, string t)
+    {
+        if (s.Length != t.Length)
+            return false;
+
+        int[] letters = new int[26];
+        foreach (char ch in s)
+            letters[ch - 'a']++;
+
+        foreach (char ch in t)
+        {
+            if (letters[ch - 'a'] == 0)
+                return false;
+            letters[ch - 'a']--;
+        }
+        return true;
+    }
+}
