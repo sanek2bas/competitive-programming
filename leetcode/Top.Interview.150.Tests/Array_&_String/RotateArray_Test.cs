8e3cfc1f4e55e4121b9f1895133b1928f5500706
sanek2bas <sanek_2bas@mail.ru>
2025-09-29 10:33:03 +0300

    RotateArray

---
+++ leetcode/Top.Interview.150.Tests/Array_&_String/RotateArray_Test.cs
@@ -0,0 +1,23 @@
+﻿using Top.Interview._150.Tests.Array___String;
+
+namespace Array___String;
+
+internal class RotateArray_Test
+{
+    [Test]
+    [MethodDataSource(nameof(DataSource))]
+    public async Task RotateArray(int[] nums, int k, int[] answer)
+    {
+        var solution = new RotateArray();
+        solution.Execute(nums, k);
+        await Assert.That(nums).IsEquivalentTo(answer);
+    }
+
+    public static IEnumerable<(int[] nums, int k, int[] answer)> DataSource()
+    {
+        yield return (new int[] { 1, 2, 3, 4, 5, 6, 7 }, 3,
+                      new int[] { 5, 6, 7, 1, 2, 3, 4 });
+        yield return (new int[] { -1, -100, 3, 99 }, 2,
+                      new int[] { 3, 99, -1, -100 });
+    }
+}
---
+++ leetcode/Top.Interview.150/Array_&_String/RotateArray.cs
@@ -0,0 +1,32 @@
+﻿namespace Top.Interview._150.Tests.Array___String;
+
+public class RotateArray
+{
+    /// <summary>
+    /// # 189
+    /// https://leetcode.com/problems/rotate-array/description/
+    /// Given an integer array nums, 
+    /// rotate the array to the right by k steps,
+    /// where k is non-negative.
+    /// </summary>
+    public void Execute(int[] nums, int k)
+    {
+        k %= nums.Length;
+        Reverse(nums, 0, nums.Length - 1);
+        Reverse(nums, 0, k - 1);
+        Reverse(nums, k, nums.Length - 1);
+    }
+
+    private void Reverse(int[] nums, int left, int right)
+    {
+        while (left < right)
+            Swap(nums, left++, right--);
+    }
+
+    private void Swap(int[] nums, int i, int j)
+    {
+        var temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
