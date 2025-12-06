4cded6c5fb05314c0cc7333b4e92d444df2544f8
sanek2bas <sanek_2bas@mail.ru>
2025-12-06 15:30:27 +0300

    AddBinaryTest

---
+++ leetcode/Top.Interview.150.Tests/Array_&_String/AddBinaryTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using TopInterview150.Array___String;
+
+namespace Array___String;
+
+public class AddBinaryTest
+{
+    [Test]
+    [Arguments("11", "1", "100")]
+    [Arguments("1010", "1011", "10101")]
+    public async Task AddBinary(string a, string b, string answer)
+    {
+        var solution = new AddBinary();
+        var result = solution.Execute(a, b);
+        await Assert.That(result).IsEqualTo(answer);
+    }    
+}
---
+++ leetcode/Top.Interview.150/Array_&_String/AddBinary.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace TopInterview150.Array___String;
+
+public class AddBinary
+{
+    /// <summary>
+    /// # 67
+    /// https://leetcode.com/problems/add-binary/description/
+    /// Given two binary strings a and b, return their sum as a binary string.
+    /// </summary>
+    public string Execute(string a, string b)
+    {
+        var answer = new StringBuilder();
+        int carry = 0;
+        int aIdx = a.Length - 1;
+        int bIdx = b.Length - 1;
+        while(aIdx >= 0 || bIdx >= 0 || carry > 0)
+        {
+            if (aIdx >= 0)
+                carry += a[aIdx--] - '0';
+            if (bIdx >= 0)
+                carry += b[bIdx--] - '0';
+            answer.Append(carry % 2);
+            carry /= 2;
+        }
+
+        var answerCharArray = answer.ToString().ToCharArray();
+        Array.Reverse(answerCharArray);
+        return new string(answerCharArray);
+    }
+}
