117338b7203e026905edea62aff1dd6affd6580e
sanek2bas <sanek_2bas@mail.ru>
2025-12-11 17:53:10 +0300

    refactoring

--- leetcode/Top.Interview.150.Tests/Data/DataClass.cs
+++ leetcode/Top.Interview.150.Tests/Data/DataClass.cs
@@ -6,11 +6,17 @@ public class DataClass : IAsyncInitializer, IAsyncDisposable
 {
     public Task InitializeAsync()
     {
-        return Console.Out.WriteLineAsync("Classes can be injected into tests, and they can perform some initialisation logic such as starting an in-memory server or a test container.");
+        return Console.Out.WriteLineAsync(
+            @"Classes can be injected into tests, 
+            and they can perform some initialisation 
+            logic such as starting an in-memory server 
+            or a test container.");
     }
 
     public async ValueTask DisposeAsync()
     {
-        await Console.Out.WriteLineAsync("And when the class is finished with, we can clean up any resources.");
+        await Console.Out.WriteLineAsync(
+            @"And when the class is finished with, 
+            we can clean up any resources.");
     }
 }
