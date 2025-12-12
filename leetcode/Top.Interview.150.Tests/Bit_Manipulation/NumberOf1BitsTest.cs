5fe74019f90039d5128176a5da172048e58d9a27
sanek2bas <sanek_2bas@mail.ru>
2025-12-12 12:46:54 +0300

    NumberOf1Bits

---
+++ leetcode/Top.Interview.150.Tests/Bit_Manipulation/NumberOf1BitsTest.cs
@@ -0,0 +1,17 @@
+using Top.Interview._150.Bit_Manipulation;
+
+namespace Bit_Manipulation;
+
+public class NumberOf1BitsTest
+{
+    [Test]
+    [Arguments(11, 3)]
+    [Arguments(128, 1)]
+    [Arguments(4_294_967_293, 31)]
+    public async Task Solution(uint n, int answer)
+    {
+        var solution = new NumberOf1Bits();
+        var result = solution.Execute(n);
+        await Assert.That(result).IsEqualTo(answer);
+    }
+}
---
+++ leetcode/Top.Interview.150/Bit_Manipulation/NumberOf1Bits.cs
@@ -0,0 +1,24 @@
+﻿namespace Top.Interview._150.Bit_Manipulation;
+
+public class NumberOf1Bits
+{
+    /// <summary>
+    /// # 191
+    /// https://leetcode.com/problems/number-of-1-bits/description/
+    /// Write a function that takes the binary representation of an 
+    /// unsigned integer and returns the number of '1' bits 
+    /// it has (also known as the Hamming weight).
+    /// </summary>
+    public int Execute(uint n)
+    {
+        //return BitOperations.PopCount(n);
+        int answer = 0;
+        foreach(int i in Enumerable.Range(0, 32))
+        {
+            if (((n >> i) & 1) == 1)
+                answer++;
+        }
+        return answer;
+
+    }
+}
