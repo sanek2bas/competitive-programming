ee8d9fab37021b5405ba01c238dc16f83326f441
sanek2bas <sanek_2bas@mail.ru>
2025-12-17 23:37:23 +0300

    CopyListWithRandomPointer

---
+++ leetcode/Top.Interview.150.Tests/Linked_List/CopyListWithRandomPointerTest.cs
@@ -0,0 +1,53 @@
+using Top.Interview._150.Common;
+using Top.Interview._150.Linked_List;
+
+namespace  Linked_List;
+
+public class CopyListWithRandomPointerTest
+{
+    [Test]
+    [MethodDataSource(nameof(DataSource))]
+    public async Task Solution(ListNodeWithRandom head, ListNodeWithRandom answer)
+    {
+        var solution = new CopyListWithRandomPointer();
+
+        var result = solution.Execute(head);
+
+        await Assert.That(CheckResult(head, answer)).IsTrue();
+    }
+
+    public IEnumerable<(ListNodeWithRandom head, ListNodeWithRandom answer)> DataSource()
+    {
+        var node1 = ListNodeWithRandom.Create(
+            (7, -1), (13, 0), (11, 4), (10, 2), (1, 0));
+        yield return (node1, node1);
+        
+        var node2 = ListNodeWithRandom.Create(
+            (1, 1), (2, 1));
+        yield return (node2, node2);
+
+        var node3 = ListNodeWithRandom.Create(
+            (3, -1), (3, 0), (3, -1));
+        yield return (node3, node3);
+    }
+
+    public bool CheckResult(ListNodeWithRandom result, ListNodeWithRandom answer)
+    {
+        if (ReferenceEquals(result, answer))
+            return false;
+
+        while (result != null && answer != null)
+        {
+            if(result.Value != answer.Value)
+                return false;
+            if (result.Next?.Value != answer.Next?.Value)
+                return false;
+            if(result.Random?.Value != result.Random?.Value)
+                return false;
+            result = result.Next;
+            answer = answer.Next;
+        }
+
+        return result == null && answer == null;
+    }
+}
---
+++ leetcode/Top.Interview.150/Common/ListNodeWithRandom.cs
@@ -0,0 +1,44 @@
+﻿namespace Top.Interview._150.Common;
+
+public class ListNodeWithRandom
+{
+    public int Value { get; init; }
+
+    public ListNodeWithRandom? Next { get; set; }
+
+    public ListNodeWithRandom? Random { get; set; }
+
+    public ListNodeWithRandom(int value = -1)
+    {
+        Value = value;
+    }
+
+    public static ListNodeWithRandom? Create(params (int value, int randomIndex)[] numbers)
+    {
+        var startNode = new ListNodeWithRandom();
+        var list = new List<ListNodeWithRandom>();
+        list.Add(startNode);
+        var withoutRandoms = new List<(ListNodeWithRandom node, int randomIndex)>();
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            var value = numbers[i].value;
+            var randomIndex = numbers[i].randomIndex;
+
+            var node = new ListNodeWithRandom(value);
+            list[i].Next = node;
+            if (randomIndex != -1)
+            {
+                if (randomIndex < list.Count-1)
+                    node.Random = list[randomIndex+1];
+                else
+                    withoutRandoms.Add(new(node, randomIndex));
+            }
+            list.Add(node);
+        }
+
+        foreach(var withoutRandom in withoutRandoms)
+            withoutRandom.node.Random = list[withoutRandom.randomIndex];
+
+        return startNode.Next;
+    }
+}
