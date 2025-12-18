7385b961b28360ce5fd810027e7c0bba17516867
sanek2bas <sanek_2bas@mail.ru>
2025-12-18 09:22:56 +0300

    added TreeNode

--- leetcode/BinaryTree/MaximumDepthOfBinaryTree.cs
+++ leetcode/BinaryTree/MaximumDepthOfBinaryTree.cs
@@ -4,17 +4,7 @@ namespace TopInterview150.BinaryTreeGeneral
 {
     public static class MaximumDepthOfBinaryTree
     {
-        /// <summary>
-        /// Given the root of a binary tree, return its maximum depth.
-        /// A binary tree's maximum depth is the number of nodes along 
-        /// the longest path from the root node down to the farthest leaf node.
-        /// </summary>
-        public static int Execute(TreeNode root)
-        {
-            if (root == null)
-                return 0;
-            return 1 + Math.Max(Execute(root.Left), Execute(root.Right));
-        }
+        
 
         public static IEnumerable<(TreeNode root, int answer)> GetTests()
         {
---
+++ leetcode/Top.Interview.150/Common/TreeNode.cs
@@ -0,0 +1,89 @@
+namespace Top.Interview._150.Binary_Tree;
+
+public class TreeNode<T>
+{
+    public T Value { get; init; }
+
+    public TreeNode<T?> Left { get; set; }
+
+    public TreeNode<T?> Right { get; set; }
+
+    private TreeNode(T value)
+    {
+        Value = value;
+    }
+
+    public static TreeNode<T?>? Map(params T?[] values)
+    {
+        if (values == null
+            || values.Length == 0
+            || values[0] == null)
+            return null;
+
+        var rootNode = new TreeNode<T?>(values[0]);
+
+        var queue = new Queue<TreeNode<T?>>();
+        queue.Enqueue(rootNode);
+        int i = 1;
+
+        while (i < values.Length)
+        {
+            var value = values[i];
+            var node = queue.Dequeue();
+            if (value != null)
+            {
+                var leftNode = new TreeNode<T?>(value);
+                queue.Enqueue(leftNode);
+                node.Left = leftNode;
+            }
+            i++;
+            if (i >= values.Length)
+                break;
+            value = values[i];
+            if (value != null)
+            {
+                var rightNode = new TreeNode<T?>(value);
+                queue.Enqueue(rightNode);
+                node.Right = rightNode;
+            }
+            i++;
+        }
+        return rootNode;
+    }
+
+    public static T?[] ToArray(TreeNode<T?> root)
+    {
+        var answer = new List<T?>();
+        if (root != null)
+        {
+            answer.Add(root.Value);
+            var queue = new Queue<TreeNode<T?>>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                if (node.Left == null && node.Right == null)
+                    continue;
+                if (node.Left != null)
+                {
+                    answer.Add(node.Left.Value);
+                    queue.Enqueue(node.Left);
+                }
+                else
+                {
+                    answer.Add(default);
+                }
+                if (node.Right != null)
+                {
+                    answer.Add(node.Right.Value);
+                    queue.Enqueue(node.Right);
+                }
+                else
+                {
+                    answer.Add(default);
+                }
+            }
+        }
+        return answer.ToArray();
+    }
+}
