d96f495f259b3e92231460113744b1684f85d45f
sanek2bas <sanek_2bas@mail.ru>
2026-03-03 08:52:30 +0300

    refactoring

--- leetcode/Top.Interview.150/Multidimensional_DP/EditDistance.cs
+++ leetcode/Top.Interview.150/Multidimensional_DP/EditDistance.cs
@@ -14,6 +14,34 @@ public class EditDistance
     /// </summary>
     public int Execute(string word1, string word2)
     {
+        var rows = word1.Length;
+        var cols = word2.Length;
+
+        var dp = new int[rows + 1, cols + 1];
+        for (int i = 0; i <= rows; i++)
+            dp[i, 0] = i;
+        for (int j = 0; j <= cols; j++)
+            dp[0, j] = j;
         
+        for (int row = 1; row <= rows; row++)
+        {
+            for (int col = 1; col <= cols; col++)
+            {
+                int prevDiag = dp[row-1, col-1];
+                if (word1[row-1] == word2[col-1])
+                {
+                    dp[row, col] = prevDiag;
+                }
+                else
+                {
+                    int prevLeft = dp[row-1, col];
+                    int prevTop = dp[row, col-1];
+                    dp[row, col] = 1 +
+                        Math.Min(prevDiag, Math.Min(prevLeft, prevTop));
+                }
+            }
+        }
+
+        return dp[rows, cols];
     }
 }
