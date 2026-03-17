4867c717756ae2132b85de7a7e5f54f8e239bd89
sanek2bas <sanek_2bas@mail.ru>
2026-03-17 14:08:07 +0300

    begin of script

---
+++ leetcode/Database.Problems/Sales_Person/init.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS SalesPerson
+CREATE TABLE SalesPerson (
+    sales_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    salary INT,
+    commission_rate INT,
+    hire_date DATE
+);
+
+DROP TABLE IF EXISTS Company
+CREATE TABLE Company (
+    com_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    city VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS Orders
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    order_date DATE,
+    com_id INT,
+    sales_id INT,
+    amount INT,
+    FOREIGN KEY (com_id) REFERENCES Company(com_id),
+    FOREIGN KEY (sales_id) REFERENCES SalesPerson(sales_id)
+);
+
+
+INSERT INTO SalesPerson (sales_id, name, salary, commission_rate, hire_date) 
+VALUES
+(1, 'John', 100000, 6, '2006-04-01'),
+(2, 'Amy', 12000, 5, '2010-05-01'),
+(3, 'Mark', 65000, 12, '2008-12-25'),
+(4, 'Pam', 25000, 25, '2005-01-01'),
+(5, 'Alex', 5000, 10, '2007-02-03');
+
+INSERT INTO Company (com_id, name, city) 
+VALUES
+(1, 'RED', 'Boston'),
+(2, 'ORANGE', 'New York'),
+(3, 'YELLOW', 'Boston'),
+(4, 'GREEN', 'Austin');
+
+INSERT INTO Orders (order_id, order_date, com_id, sales_id, amount) 
+VALUES
+(1, '2014-01-01', 3, 4, 10000),
+(2, '2014-02-01', 4, 5, 5000),
+(3, '2014-03-01', 1, 1, 50000),
+(4, '2014-04-01', 1, 4, 25000);
--- leetcode/Database.Problems/Sales_Person/main.py
+++ leetcode/Database.Problems/Sales_Person/main.py
@@ -1,12 +1,31 @@
+import os
 import psycopg2 as pc
 
 database_url = "postgresql://postgres:postgres@localhost:8080/test"
 
+def read_sql_file(filename):
+    abs_path = os.path.abspath(filename)
+    base_dir = os.path.dirname(abs_path)
+    file_path = os.path.join(base_dir, filename)
+    with open(file_path, 'r') as file:
+        sql_query = file.read()
+    return sql_query
+
 try:
+    query_init = read_sql_file("init.sql")
     with pc.connect(database_url) as conn:
         print("connection succesful")
+        cursor = conn.cursor
+
+        cursor.execute(query_init)
+        conn.commit()
+
+        cursor.execute("SELECT * FROM SalesPerson")
+        print(cursor.fetchall())
+
+        cursor.close()
+        conn.close()
 except Exception as e:
     print(f'Error: {e}')
-
 
 print('Hello world')
