e75d05087c1a71a568a775891b3838fe4325ec61
sanek2bas <sanek_2bas@mail.ru>
2026-03-30 12:55:42 +0300

    refactoring sales_person

--- leetcode/Database.Problems/Sales_Person/main.py
+++ leetcode/Database.Problems/Sales_Person/main.py
@@ -1,10 +1,20 @@
-import common
+import os
+import sys
+from pathlib import Path
+
+current_dir = Path(__file__).resolve().parent
+parent_folder = current_dir.parent
+sys.path.insert(0, str(parent_folder))
+
+from utils import *
 
 try:
-    init_query = common.read_sql_file("init.sql")
-    solution_query = common.read_sql_file("solution.sql")
+    init_sql_path = os.path.join(current_dir, 'init.sql')
+    init_query = read_sql_file(init_sql_path)
+    solution_sql_path = os.path.join(current_dir, 'solution.sql')
+    solution_query = read_sql_file(solution_sql_path)
 
-    connection = common.get_connection()
+    connection = get_connection()
     cursor = connection.cursor()
 
     cursor.execute(init_query)
