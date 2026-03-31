0b177fe310116047bc6556772de3b5524217b2f6
sanek2bas <sanek_2bas@mail.ru>
2026-03-31 15:27:41 +0300

    refactoring

--- leetcode/Database.Problems/Sales_Person/main.py
+++ leetcode/Database.Problems/Sales_Person/main.py
@@ -8,30 +8,7 @@ sys.path.insert(0, str(parent_folder))
 
 from utils import *
 
-try:
-    init_sql_path = os.path.join(current_dir, 'init.sql')
-    init_query = read_sql_file(init_sql_path)
-    solution_sql_path = os.path.join(current_dir, 'solution.sql')
-    solution_query = read_sql_file(solution_sql_path)
+init_sql_path = os.path.join(current_dir, 'init.sql')
+solution_sql_path = os.path.join(current_dir, 'solution.sql')
 
-    connection = get_connection()
-    cursor = connection.cursor()
-
-    cursor.execute(init_query)
-    connection.commit()
-
-    cursor.execute(solution_query)
-    connection.commit()
-
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-    cursor.close()
-
-except Exception as e:
-    print(f'Error: {e}')
-
-finally:
-    if connection:
-        connection.close()
+execute_sql_files(init_sql_path, solution_sql_path);
--- leetcode/Database.Problems/utils.py
+++ leetcode/Database.Problems/utils.py
@@ -1,11 +1,39 @@
 import psycopg2 as pc
 
-database_url = "postgresql://postgres:postgres@localhost:8080/test"
+_database_url = "postgresql://postgres:postgres@localhost:8080/test"
 
-def read_sql_file(file_path):
+def execute_sql_files(init_sql_path, solution_sql_path):
+    try:
+        init_query = _read_sql_file(init_sql_path)
+        solution_query = _read_sql_file(solution_sql_path)
+
+        connection = _get_connection()
+        cursor = connection.cursor()
+
+        _execute_sql(connection, cursor, init_query);
+        _execute_sql(connection, cursor, solution_query);
+
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+        cursor.close()
+
+    except Exception as e:
+        print(f'Error: {e}')
+
+    finally:
+        if connection:
+            connection.close()
+
+def _execute_sql(connection, cursor, query):
+    cursor.execute(query)
+    connection.commit()    
+
+def _read_sql_file(file_path):
     with open(file_path, 'r') as file:
         sql_query = file.read()
     return sql_query
 
-def get_connection():
-    return pc.connect(database_url)
+def _get_connection():
+    return pc.connect(_database_url)
