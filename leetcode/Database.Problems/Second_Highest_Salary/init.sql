26188cfdb2c1a55a50144873a02bec4d579947a4
sanek2bas <sanek_2bas@mail.ru>
2026-04-07 09:51:22 +0300

    two solitions

--- leetcode/Database.Problems/Second_Highest_Salary/init.sql
+++ leetcode/Database.Problems/Second_Highest_Salary/init.sql
@@ -7,7 +7,8 @@ CREATE TABLE Employee (
 INSERT INTO Employee (
     id, salary) 
 VALUES
-(1, 100),
-(2, 200),
-(3, 300);
---(1, 100);
+--(1, 100),
+--(2, 200),
+--(3, 300),
+--(4, 300);
+(1, 100);
--- leetcode/Database.Problems/Second_Highest_Salary/solution.sql
+++ leetcode/Database.Problems/Second_Highest_Salary/solution.sql
@@ -1,3 +1,18 @@
-SELECT salary as SecondHighestSalary
-FROM Employee
-ORDER BY 1;
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) as SecondHighestSalary;
+
+/*
+SELECT (
+    SELECT DISTINCT salary
+    FROM (
+        SELECT salary,
+        DENSE_RANK() OVER (ORDER BY salary DESC) ranking
+        FROM Employee
+    )
+    WHERE ranking = 2
+) as SecondHighestSalary
+*/
