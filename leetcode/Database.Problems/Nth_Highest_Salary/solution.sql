ba576a5a68a09cd73e7d16a5bb1203f42b8de9db
sanek2bas <sanek_2bas@mail.ru>
2026-05-13 10:03:54 +0300

    NthHighestSalary function

--- leetcode/Database.Problems/Nth_Highest_Salary/solution.sql
+++ leetcode/Database.Problems/Nth_Highest_Salary/solution.sql
@@ -1,17 +1,18 @@
 CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) 
-RETURNS TABLE (salary INT)
+RETURNS TABLE (Salary INT)
 AS $$
 BEGIN
+    IF N <= 0 THEN
+        Salary := NULL;
+        RETURN;
+    END IF;
+
     RETURN QUERY (
-        SELECT DISTINCT ranked.salary
-        FROM (
-            SELECT salary,
-            DENSE_RANK() OVER (ORDER BY salary DESC) ranking
-            FROM Employee
-        ) as ranked
-        WHERE ranking = N
-    );
+        SELECT DISTINCT e.salary
+        FROM Employee e
+        ORDER BY e.salary DESC
+        OFFSET (N-1) LIMIT 1);
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT * FROM NthHighestSalary(2);
+SELECT * FROM NthHighestSalary(1);
